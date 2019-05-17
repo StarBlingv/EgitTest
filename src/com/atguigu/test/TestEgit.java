b5e6bc1b57d68fe2e4a41d22fe5a9da426f50bfc
StarBlingv <1210070346@qq.com>
2019-05-17 17:53:22 +0800

    更新1

--- src/com/atguigu/test/TestEgit.java
+++ src/com/atguigu/test/TestEgit.java
@@ -3,7 +3,6 @@ package com.atguigu.test;
 public class TestEgit {
 	public static void main(String[] args) {
 		System.out.println("aa");
-		System.err.println("bb");
-		System.err.println("cc");
+		
 	}
 }
